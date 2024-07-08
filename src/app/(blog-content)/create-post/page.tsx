2d5c046417f0174b104890a8be84eb418d0a8bd6
Somdev <somdev4g@gmail.com>
2024-07-08 16:33:42 +0530

    Post creation working - basic

--- src/app/(blog-content)/create-post/page.tsx
+++ src/app/(blog-content)/create-post/page.tsx
@@ -1,13 +1,37 @@
 "use client";
+import ContentServices from "@/services/content.services";
+import { useMutation } from "@tanstack/react-query";
 import React, { useState } from "react";
+import Tags from "../../../componants/tags/tags";
 
 const CreatePost = () => {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+  const createPostMutation = useMutation({
+    mutationFn: (payload: any) => ContentServices.createPost(payload),
+    onSuccess: (response: any) => {
+      console.log(response);
+    },
+    onError: (error: any) => {
+      console.error("Create post error:", error);
+    },
+  });
+
+  // const handleTagsSelection = (selectedTags: string) => {
+  //   console.log("Selected tags:", selectedTags);
+  // };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Title:", title);
-    console.log("Content:", content);
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("tags", "1, 2");
+
+    console.log(formData);
+    createPostMutation.mutate(formData);
   };
 
   return (
@@ -33,6 +57,7 @@ const CreatePost = () => {
           }}
         ></textarea>
       </div>
+      {/* <Tags onSelectTags={handleTagsSelection} /> */}
       <button type="submit">Submit</button>
     </form>
   );
---
+++ src/services/content.services.ts
@@ -0,0 +1,23 @@
+import { isAxiosError } from "axios";
+import axios from "@/configs/axios.config";
+import { getLocalValue } from "@/utils/localStorage.utils";
+
+const ContentServices = {
+  async createPost(data: any) {
+    const token = localStorage.getItem("jwt");
+    try {
+      const response = await axios.post("create-post", data, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      return response;
+    } catch (error: any) {
+      if (isAxiosError(error)) {
+        throw new Error(error.response?.data.message);
+      }
+      throw new Error("Something went wrong. Please try again.");
+    }
+  },
+};
+export default ContentServices;
