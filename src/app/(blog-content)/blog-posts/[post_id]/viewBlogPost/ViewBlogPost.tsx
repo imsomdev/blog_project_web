ef06e0397d586b0999f5bfc07947df9377d91863
Somdev <somdev4g@gmail.com>
2024-07-11 16:41:40 +0530

    design updated

--- src/app/(blog-content)/blog-posts/[post_id]/viewBlogPost/ViewBlogPost.tsx
+++ src/app/(blog-content)/blog-posts/[post_id]/viewBlogPost/ViewBlogPost.tsx
@@ -1,9 +1,10 @@
-"use client";
 import DeletePost from "@/componants/deletePost/DeletePost";
 import EditPost from "@/componants/editPost/EditPost";
+import LoadingSpinner from "@/componants/loadingSpinner/LoadingSpinner";
 import ContentServices from "@/services/content.services";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import styles from "./ViewBlogPost.module.css";
 
 interface BlogPostInterface {
   params: {
@@ -16,27 +17,36 @@ const ViewBlogPost = ({ params }: BlogPostInterface) => {
     queryKey: ["blog-post-view", params.post_id],
     queryFn: () => ContentServices.getPostById(params.post_id),
   });
-  console.log(data);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={styles.loadingSpinner}>
+        <LoadingSpinner />
+      </div>
+    );
   }
 
   if (isError) {
-    return <div>Error: {error.message}</div>;
+    return <div className="styles.error-message">Error: {error.message}</div>;
   }
 
   return (
-    <div>
-      <h1>{data?.title}</h1>
-      <p>{data?.content}</p>
-      Tags:{" "}
-      {data?.tags.map((item: any) => (
-        <span key={item.id}>{item.name}, </span>
-      ))}
-      <p>Author: {data?.author}</p>
-      {data && <EditPost postId={params.post_id} data={data} />}
-      {data && <DeletePost postId={params.post_id} data={data} />}
+    <div className={styles.gridContainer}>
+      <div className={styles.blogPostContent}>
+        <h1>{data?.title}</h1>
+        <p>{data?.content}</p>
+        <div className={styles.tags}>
+          Tags:{" "}
+          {data?.tags.map((item: any) => (
+            <span key={item.id}>{item.name}, </span>
+          ))}
+        </div>
+        <p className={styles.author}>Author: {data?.author}</p>
+        <div className={styles.editDeleteBtns}>
+          {data && <EditPost postId={params.post_id} data={data} />}
+          {data && <DeletePost postId={params.post_id} data={data} />}
+        </div>
+      </div>
     </div>
   );
 };
--- src/componants/blogPosts/BlogPosts.tsx
+++ src/componants/blogPosts/BlogPosts.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useToken } from "@/context/TokenContext";
 import ContentServices from "@/services/content.services";
-import { getLocalValue } from "@/utils/localStorage.utils";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import React, { useState, useEffect } from "react";
+import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
+import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
+import styles from "./BlogPosts.module.css";
 
 const BlogPosts = () => {
   const router = useRouter();
@@ -16,32 +17,56 @@ const BlogPosts = () => {
     retry: 1,
     refetchOnWindowFocus: false,
   });
-
-  if (isLoading) return <div>Loading...</div>;
+  LoadingSpinner();
+  if (isLoading)
+    return (
+      <div>
+        <LoadingSpinner />
+      </div>
+    );
   if (isError) return <div>Something went worng</div>;
 
   const handleReadPost = (id: string) => {
     router.push(`/blog-posts/${id}`);
   };
+  console.log(data, "BLOGPOST");
 
   return (
     token && (
-      <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold text-center mb-8">Blog Posts</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {data?.map((post: any) => (
-            <div key={post.id} className="bg-white rounded-lg shadow-md p-6">
-              <h2 className="text-2xl font-semibold mb-4">{post.title}</h2>
-              <p className="text-gray-700 mb-4">{post.excerpt}</p>
-              <button
+      <div className={styles.gridContainer}>
+        {data.map((post: any) => (
+          <Card
+            key={post.id}
+            isFooterBlurred
+            className="w-full h-[300px] col-span-12 sm:col-span-7 md:col-span-4 lg:col-span-3"
+          >
+            <CardHeader className="absolute z-10 top-1 flex-col items-start">
+              <h4 className="text-white font-medium text-xl overflow-hidden whitespace-nowrap text-ellipsis">
+                {post.title}
+              </h4>
+            </CardHeader>
+            <Image
+              removeWrapper
+              alt="Relaxing app background"
+              className="z-0 w-full h-full object-cover"
+              src="https://nextui.org/images/card-example-2.jpeg"
+            />
+            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+              <div className="flex flex-grow gap-2 items-center">
+                {" "}
+                <p className="text-tiny text-white">Posted by </p>
+                <p className="text-base text-white">{post?.author}</p>
+              </div>
+              <Button
+                radius="full"
+                size="sm"
                 onClick={() => handleReadPost(post.id)}
-                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200"
               >
                 Read Post
-              </button>
-            </div>
-          ))}
-        </div>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     )
   );
